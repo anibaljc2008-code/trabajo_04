e5917b41b7910ebfa2d04215c873f1a6a1b21009
anibaljc2008-code <anibaljc2008@gmail.com>
2025-10-15 12:25:07 -0600

    Add files via upload

---
+++ trabajo_02.py
@@ -0,0 +1,21 @@
+# PROGRAMA: calculadora de suma y resta
+# autor: [tu nombre]
+# fecha: [fecha]
+
+print("=== calculadora de suma y resta ===")
+
+# leer los numeros desde tu teclado
+numero1 = float(input("ingrese el primer numero: "))
+numero2 = float(input("ingrese el segundo numero: "))
+
+# calcula la suma y la resta de los numeros puestos
+suma = numero1 + numero2
+resta = numero1 - numero2
+
+# mostrar los resultados optenidos
+print("resultado")
+print("la suma de", numero1, "+", numero2, "es:", suma)
+print("la suma de", numero1, "-", numero2, "es:", resta)
+
+# final
+input("presione enter para salir...")
