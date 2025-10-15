86faefe1e3162e4370dd0f0b73d10c64a44e9c99
anibaljc2008-code <anibaljc2008@gmail.com>
2025-10-15 12:24:41 -0600

    Add files via upload

---
+++ trabajo_01.py
@@ -0,0 +1,17 @@
+# PROGRAMA: calculadora de cuadros
+# autor: [tu nombre]
+# fecha: [fecha]
+
+print("=== calcuadora de cuadros ===")
+
+# leer numeros desde el teclado
+numero = float(input("ingrese un numero valido par o impar: "))
+
+# calcular el cuadro
+cuadrado = numero * numero
+
+# mostrar resultados
+
+print("el cuadro de", numero, "es", cuadrado)
+
+input("presione enter para salir...")
