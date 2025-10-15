20b979e1b35914fe6d32d05186418db7178355a8
anibaljc2008-code <anibaljc2008@gmail.com>
2025-10-15 12:25:37 -0600

    Add files via upload

---
+++ trabajo_03.py
@@ -0,0 +1,8 @@
+tc = int(input('ingrese la temperatura en grados celsuis: '))
+
+tf = (tc * 9/7) + 32
+
+print(f'{tc}grados celsuis son {tf}grados fahrenheit')
+
+# final
+input("presione enter para salir...")
